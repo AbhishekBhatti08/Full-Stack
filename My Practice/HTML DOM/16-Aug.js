41f1db0ee1c2b83f2435d3176b5b0964c8442b70
Abhishek Bhatti <abhbhatt3@publicisgroupe.net>
2020-08-19 11:13:40 +0530

    JS strings and Array

---
+++ My Practice/HTML DOM/16-Aug.js
@@ -0,0 +1,17 @@
+
+var a = document.getElementsByClassName("container");
+
+console.log(a);
+
+function abc () {
+
+    var c = document.getElementsByTagName("h2")
+
+    c[0].innerHTML = "Hello";
+}
+
+
+
+var z = document.querySelectorAll("div")
+
+console.log(z);
