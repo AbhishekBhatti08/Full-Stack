35c9b12e33cb36a6fb46158f33b2d2a9fb86e576
AbhishekBhatti08 <abhishekbhatti89@gmail.com>
2020-09-17 21:50:09 +0530

    city app

--- JS Backend Development/myApp.js
+++ JS Backend Development/myApp.js
@@ -5,49 +5,24 @@ const express = require("express");
 var app = express();
 var port = 3000;
 
-app.use(bodyParser.urlencoded({extended:true}));
-
-app.use(express.static("public"))
-
+app.use(express.static("public")); // to specify to look out static assets in public folder.
 app.set("view engine","ejs");
 
-
-app.get("/",(req,res)=>{
-  res.render("home")
+app.get("/",(req,res) => {
+    
+    res.render("city");
+    
 })
 
-var city = [{
-  name:"gurgaon",
-  imgUrl:""
-},{
-  name:"delhi",
-  imgUrl:""
-},{
-  name:"",
-  imgUrl:""
-},{
-  name:"Mojito",
-  imgUrl:""
-}];
+app.get("/europe",(req,res)=>{
+    res.render("eu")
+  })
 
-
-
-
-// app.get("/",(req,res) => {
-    
-//     res.render("city");
-    
-// })
-
-// app.get("/europe",(req,res)=>{
-//     res.render("eu")
-//   })
-
-// app.get("/europe/:london",(req,res)=>{
-//     var x = req.params.london;
-//     res.render("london",{city:x});
-//   })
+app.get("/europe/:london",(req,res)=>{
+    var x = req.params.london;
+    res.render("london",{city:x});
+  })
   
-// app.listen(port, function(){
-//     console.log('server running on '+ port);
-// })
+app.listen(port, function(){
+    console.log('server running on '+ port);
+})
