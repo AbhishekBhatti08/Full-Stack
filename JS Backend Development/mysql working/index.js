a96e8d17e9155a1ddccad1f6e4dad7e8418ee33e
Abhishek Bhatti <abhbhatt3@publicisgroupe.net>
2020-11-01 01:30:55 +0530

    Twitter project working

--- JS Backend Development/mysql working/index.js
+++ JS Backend Development/mysql working/index.js
@@ -1,16 +1,15 @@
-const express = require("express")
+const express = require("express");
 const mysql  = require("mysql");
 const app = express();
 
 const port = 4000;
 app.set("view engine","ejs");
 
-
 // creating db connection
 const db = mysql.createConnection({
     host:"localhost",
     user:"root",
-    password:"BMW@mysql",
+    password:"1234",
     database:"emp"
 });
 db.connect((err)=>{
@@ -23,38 +22,42 @@ db.connect((err)=>{
 });
 
 var BEdata;
-var query  = `insert into empDetails(name, age) value ("Divyajeet", 23);`;
+var query  = `insert into empDetails(name, age) value ("Aman", 45);`;
 var query2 = `select * from empDetails;`;
 var jsonData = {
     name: "vikas",
     age:54
 }
-var query3 = `insert into empDetails set ${jsonData}`;
 
-// db.query(query3,jsonData,(err,results)=>{
-//     if(err)
-//         console.log(err);
-//     else{
-//        BEdata = results;
-//        console.log("data saved!")
-//     }
-        
-// });
+var query3 = `insert into empdetails SET ?;`;
 
-db.query(query2,(err,results)=>{
+db.query(query3,jsonData,(err,results)=>{
     if(err)
         console.log(err);
     else{
        BEdata = results;
+       console.log("data saved!")
     }
         
 });
+
+// db.query(query,(err,results)=>{
+//     if(err)
+//         console.log(err);
+//     else{
+//        BEdata = results;
+//        console.log(results);
+//     }
+        
+// });
+
 app.get("/",(req,res)=>{
     res.send("everything good!")
 })
 
 app.get('/showResults',(req,res)=>{
     res.render("showResults",{FEdata:BEdata});
+    console.log(FEdata);
 })
 app.listen(port,function(){
     console.log(`app running on ${port}`)
