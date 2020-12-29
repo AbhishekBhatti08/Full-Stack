886ae1e1560b6c91b3a1fdc961eace75ed627906
Abhishek Bhatti <abhbhatt3@publicisgroupe.net>
2020-12-29 19:32:05 +0530

    React Website Completed

--- Projects/react static website/my-app/src/App.js
+++ Projects/react static website/my-app/src/App.js
@@ -4,6 +4,9 @@ import React from 'react';
 import { BrowserRouter as Router , Switch, Route } from 'react-router-dom';
 import './App.css';
 import Home from '../src/components/Pages/Home';
+import Services from '../src/components/Pages/Services';
+import Products from '../src/components/Pages/Products';
+import SignUp from '../src/components/Pages/SignUp';
 
 function App() {
   return (
@@ -12,6 +15,9 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/services" component={Services} />
+        <Route path="/products" component={Products} />
+        <Route path="/sign-up" component={SignUp} />
       </Switch>
 
       </Router>
--- Projects/react static website/my-app/src/components/Pages/Home.js
+++ Projects/react static website/my-app/src/components/Pages/Home.js
@@ -1,13 +1,15 @@
 import '../../App.css';
 import React from 'react'
 import HeroSection from '../HeroSection';
-import Cards from '../Pages/Cards'
+import Cards from '../Pages/Cards';
+import Footer from '../Footer';
 
 function Home () {
     return (
         <>
             <HeroSection />
             <Cards />
+            <Footer />
         </>
 
 
